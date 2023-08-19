3786ae597eb7c160fa3adc37c1eacac2e0e49dab
DaviVidal01 <davividal0605@gmail.com>
2023-08-19 16:50:43 -0300

    Finalizando atividade de sábado

---
+++ TemplateShop/Website/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def produtos(request):
+    return render(request,'produtos.html')
+
+def carrinho(request):
+    return render(request,'carrinho.html')
