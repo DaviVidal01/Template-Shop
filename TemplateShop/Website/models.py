8369a3eb5bc6e6e98d0988965fb168dd55c44bef
DaviVidal01 <davividal0605@gmail.com>
2023-08-23 22:20:21 -0300

    Renomeando o Models do Banco de Dados e criando um Admin

--- TemplateShop/Website/models.py
+++ TemplateShop/Website/models.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 # Create your models here.
 class Site(models.Model):
-    nome_card = models.CharField(max_length=200)
+    nome_produto = models.CharField(max_length=200)
     description = models.TextField()
     path = models.TextField()
-    primeiro_texto = models.TextField()
-    segundo_texto = models.TextField()
-    usuario = models.IntegerField()
+    preco_produto = models.DecimalField(max_digits=5, decimal_places=2)
+    imagem_produto = models.ImageField()
+    usuario = models.CharField(max_length=20)
     date_create = models.DateTimeField(default=datetime.now, blank = True)
