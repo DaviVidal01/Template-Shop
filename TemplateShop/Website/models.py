977340d4215eed8bfa2691fa8650a1dc5eab6cca
DaviVidal01 <davividal0605@gmail.com>
2023-08-29 21:05:53 -0300

    Finalizando as configurações dos models e adicionando pasta Images nas URLs

--- TemplateShop/Website/models.py
+++ TemplateShop/Website/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Site(models.Model):
     nome_produto = models.CharField(max_length=200)
     description = models.TextField(max_length=255)
-    path = models.ImageField()
+    path = models.ImageField( upload_to="images/")
     preco_produto = models.DecimalField(default=000.00, max_digits=5, decimal_places=2)
     usuario = models.CharField(max_length=20)
     date_create = models.DateTimeField(default=datetime.now, blank = True)
--- TemplateShop/setup/settings.py
+++ TemplateShop/setup/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR,'uploads')
-MEDIA_URL = 'uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
